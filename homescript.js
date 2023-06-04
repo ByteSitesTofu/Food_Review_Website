9f16cd72909b0a3ee2b30d6a821f12bd9b2451fb
TofuWithSoy <yamasakikaito01@gmail.com>
2023-06-04 18:43:58 +0800

    [Update - 005] Adding images and fix bugs
    Day 5 of working this website, add images and fix the text on dark mode
    
    Next step is the link of the images and adding likes, comments,
    and rating about the article.

--- homescript.js
+++ homescript.js
@@ -1,7 +1,6 @@
+// DarkMode
 const body = document.querySelector("body");
 const toggle = document.querySelector("#toggle");
-const sunIcon = document.querySelector(".toggle .fa-sun");
-const moonIcon = document.querySelector(".toggle .fa-moon");
 
 toggle.addEventListener("change", () => {
   body.classList.toggle("dark");
